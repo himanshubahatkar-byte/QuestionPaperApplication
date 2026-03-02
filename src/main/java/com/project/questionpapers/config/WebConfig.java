d81bb6e29aff4c85c63564a791703aefde3e1d1e
himanshubahatkar-byte <himanshubahatkar@gmail.com>
2026-03-02 18:42:36 +0530

    Updated backend API and fixed bugs

--- src/main/java/com/project/questionpapers/config/WebConfig.java
+++ src/main/java/com/project/questionpapers/config/WebConfig.java
@@ -10,7 +10,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:8080")
+                .allowedOrigins("https://college-mini-project-psi.vercel.app/")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*");
     }
